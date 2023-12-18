50db7411263b40202a877c7b12c9029bf7e96757
zs0732 <zs0732@bs.amalnet.k12.il>
2023-12-18 14:52:12 +0200

    kmomkokmo

--- app/src/main/java/com/example/buttonexer1/MainActivity.java
+++ app/src/main/java/com/example/buttonexer1/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.buttonexer1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+    }
+
+    public void click(View view) {
+        btn.setText("Oh,yea,I've been clicked!");
     }
 }
